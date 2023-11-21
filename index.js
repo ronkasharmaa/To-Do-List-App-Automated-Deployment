f50decc23366226d6358f49fe90563f2ad69d5c7
NightShift0607 <142142387+NightShift0607@users.noreply.github.com>
2023-11-21 23:12:34 +0530

    Database connected

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import pg from "pg";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
@@ -9,12 +10,16 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 var todayDate = "";
-var list1 = [
-    { id: 1, title: 'Be Good'},
-    { id: 2, title: 'Fuck Off' },
-    { id: 3, title: 'Love Everyone'}
-];
-var list2 = [];
+var list = [];
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "todolist",
+    password: "Rajat@0607",
+    port: 5432
+});
+db.connect();
 
 function setDate(req, res, next) {
     var month;
@@ -88,47 +93,100 @@ function setDate(req, res, next) {
 }
 app.use(setDate);
 
+async function getList1() {
+    const result = await db.query("SELECT * FROM today_items");
+    list = result.rows;
+    return list
+}
+
+async function getList2() {
+    const result = await db.query("SELECT * FROM work_items");
+    list = result.rows;
+    return list
+}
 
 
-
-app.get("/", (req,res) => {
-    let length = list1.length;
+app.get("/", async (req,res) => {
+    const list = await getList1();
+    let length = list.length;
     res.render(__dirname + "/views/index.ejs", { 
         date : todayDate,
-        values : list1,
+        values : list,
         len : length
     });
-})
+});
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
     var inputValue = req.body['newItem'];
-    list1.push(inputValue);
-    let length = list1.length;
-    res.render(__dirname + "/views/index.ejs", {
-        date : todayDate,
-        values : list1,
-        len : length
-    });
+    try {
+        await db.query("INSERT INTO today_items (title) VALUES ($1)",[inputValue]);
+        res.redirect("/")
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.post("/todedit", async (req,res) => {
+    var updateId = req.body.updatedListId;
+    var updateTitle = req.body.updatedListTitle;
+    try {
+        await db.query("UPDATE today_items SET title = $1 WHERE id = $2",[updateTitle, updateId]);
+        res.redirect("/");
+    } catch (error) {
+        console.log(error);
+    }
 })
 
-app.get("/work", (req,res) => {
-    let length = list2.length;
+app.post("/toddelete", async (req,res) => {
+    var deletedId = req.body.deletedItemId;
+    try {
+        await db.query("DELETE FROM today_items WHERE id = $1", [deletedId]);
+        res.redirect("/");
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.get("/work", async (req,res) => {
+    const list = await getList2();
+    let length = list.length;
     res.render(__dirname + "/views/work.ejs", { 
-        values : list2,
+        values : list,
         len : length
     });
-})
+});
 
-app.post("/work", (req, res) => {
+app.post("/work", async (req, res) => {
     var inputValue = req.body['newItem'];
-    list2.push(inputValue);
-    let length = list2.length;
-    res.render(__dirname + "/views/work.ejs", {
-        values : list2,
-        len : length
-    });
-})
+    try {
+        await db.query("INSERT INTO work_items (title) VALUES ($1)",[inputValue]);
+        res.redirect("/work")
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.post("/woredit", async (req,res) => {
+    var updateId = req.body.updatedListId;
+    var updateTitle = req.body.updatedListTitle;
+    try {
+        await db.query("UPDATE work_items SET title = $1 WHERE id = $2",[updateTitle, updateId]);
+        res.redirect("/work");
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.post("/wordelete", async (req,res) => {
+    var deletedId = req.body.deletedItemId;
+    try {
+        await db.query("DELETE FROM work_items WHERE id = $1", [deletedId]);
+        res.redirect("/");
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
-})
+});
